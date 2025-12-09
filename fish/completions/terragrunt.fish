358f4c13bdbc0e017780e424e8e0daa397ee7fbb
feyzee <11005389+feyzee@users.noreply.github.com>
2025-12-09 12:54:53 +0530

    fix: removing redundant fish config

--- fish/completions/terragrunt.fish
+++
@@ -1,9 +0,0 @@
-
-function __complete_terragrunt
-    set -lx COMP_LINE (commandline -cp)
-    test -z (commandline -ct)
-    and set COMP_LINE "$COMP_LINE "
-    /home/stellarexplosions/.local/bin/terragrunt
-end
-complete -f -c terragrunt -a "(__complete_terragrunt)"
-
--- fish/completions/vault.fish
+++
@@ -1,9 +0,0 @@
-
-function __complete_vault
-    set -lx COMP_LINE (commandline -cp)
-    test -z (commandline -ct)
-    and set COMP_LINE "$COMP_LINE "
-    /usr/bin/vault
-end
-complete -f -c vault -a "(__complete_vault)"
-
