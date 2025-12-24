eed2bcd2be22c7c9970b822c04ed26a43784d6d3
feyzee <11005389+feyzee@users.noreply.github.com>
2025-12-24 20:10:55 +0530

    fix: terraform lsps for nvim

--- .config/nvim/lsp/tofu_ls.lua
+++ .config/nvim/lsp/tofu_ls.lua
@@ -5,7 +5,7 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { "tofu-ls", "serve" },
+  cmd = { "tofu-ls", "serve", "-cpuprofile cpu.prof", "-memprofile mem.prof" },
   filetypes = { "opentofu", "opentofu-vars", "terraform", "terraform-vars" },
   root_markers = { ".terraform", ".git" },
 
@@ -24,4 +24,14 @@ return {
 
     return capabilities
   end)(),
+
+  on_attach = function(client, bufnr)
+    if client and client.server_capabilities then
+      -- Disable overlapping capabilities that terraform-ls should handle
+      client.server_capabilities.codeLensProvider = nil
+      client.server_capabilities.foldingRangeProvider = nil
+      client.server_capabilities.semanticTokensProvider = nil
+      client.server_capabilities.inlayHintProvider = nil
+    end
+  end,
 }
--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -69,7 +69,7 @@ local servers = {
   "ruff",
   "rust_analyzer",
   -- "tofu_ls",
-  "tflint",
+  -- "tflint",
   "ts_ls",
   "yamlls",
 }
