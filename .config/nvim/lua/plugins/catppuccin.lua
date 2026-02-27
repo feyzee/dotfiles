df2e5b6d1a4a9e3dfee6d9086c5675073c45f008
feyzee <11005389+feyzee@users.noreply.github.com>
2026-02-27 22:26:22 +0530

    fix: split themes into separate files

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,67 @@
+-- Included plugins in this module:
+--   - tokyonight.nvim
+--   - catppuccin
+
+return {
+  "catppuccin/nvim",
+  enabled = false,
+  lazy = false, -- Always load colorscheme early if enabled
+  priority = 1000,
+  name = "catppuccin",
+  opts = {
+    flavour = "macchiato", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+      light = "latte",
+      dark = "macchiato",
+    },
+    transparent_background = false,
+    term_colors = false,
+    dim_inactive = {
+      enabled = false,
+      shade = "dark",
+      percentage = 0.15,
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    styles = {
+      comments = { "italic" },
+      conditionals = { "italic" },
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+      operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      telescope = true,
+      notify = false,
+      mini = false,
+      fidget = true,
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/tokyo-night.lua
@@ -0,0 +1,5 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false, -- Always load colorscheme early
+  priority = 1000,
+}
