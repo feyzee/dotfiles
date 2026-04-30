b3e62b8a2ef906c89fe4e78c7b54c3afec0067f3
feyzee <11005389+feyzee@users.noreply.github.com>
2026-04-30 23:00:08 +0530

    fix(nvim): removed unused and redundant keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,34 +2,17 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local opts = { noremap = true, silent = true }
-
 -- keymaps for better default experience
--- See `:help vim.keymap.set()`
 vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
-vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-vim.keymap.set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 vim.keymap.set({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Fold related keymaps
-vim.keymap.set("n", "+", ":foldopen<CR>", { desc = "Open code fold" })
-vim.keymap.set("n", "-", ":foldclose<CR>", { desc = "Close code fold" })
+vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 
 -- LSP
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Goto References" })
-vim.keymap.set("n", "gI", vim.lsp.buf.implementation, { desc = "Goto Implementation" })
-vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, { desc = "Goto Type Definition" })
 vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Documentation" })
 vim.keymap.set("n", "gK", vim.lsp.buf.signature_help, { desc = "Signature Help" })
-vim.keymap.set({ "n", "v" }, "gca", vim.lsp.buf.code_action, { desc = "Code Action" })
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename Object using LSP" })
 
 -- Diagnostics
 vim.keymap.set("n", "[d", function()
@@ -42,20 +25,10 @@ vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show Diagnostics 
 vim.keymap.set("n", "gqf", vim.diagnostic.setloclist, { desc = "Show Diagnostics in Quickfix Window" })
 --
 -- Window management
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
-vim.keymap.set("n", "<S-Right>", ":vertical resize -2<CR>", opts)
-vim.keymap.set("n", "<S-Left>", ":vertical resize +2<CR>", opts)
-
--- Text
-vim.keymap.set("n", "<A-Up>", "<Esc>:m .-2<CR>==", opts)
-vim.keymap.set("n", "<A-Down>", "<Esc>:m .+1<CR>==", opts)
-
--- Terraform
-vim.keymap.set("n", "<leader>tfv", ":!terraform validate<CR>", opts)
-vim.keymap.set("n", "<leader>tfmt", ":!terraform fmt<CR>", opts)
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 
 -- Tabs
 vim.keymap.set("n", "<leader>tr", function()
